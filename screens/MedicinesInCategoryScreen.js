f0d5ca2c86db7b4cf8c7939f0c40c3612dd42ddb
peter-swe <lelkes@me.com>
2023-04-14 20:47:37 +0200

    dynamic navigation

--- screens/MedicinesInCategoryScreen.js
+++ screens/MedicinesInCategoryScreen.js
@@ -1,15 +1,26 @@
 import {FlatList, StyleSheet, Text, View} from "react-native";
-import React from "react";
+import React, {useEffect, useLayoutEffect} from "react";
 
 import {DRUGS} from "../constants/data";
 import DrugItem from "../components/DrugItem";
+import {LMCategory} from "../constants/kategorier";
 
-const MedicinesInCategoryScreen = ({route}) => {
+const MedicinesInCategoryScreen = ({route, navigation}) => {
   const categoryId = route.params.kategoriId;
 
   const displayedDrugs = DRUGS.filter((lmItem) => {
     return lmItem.catId.includes(categoryId);
   });
+
+  useLayoutEffect(() => {
+    const categoryName = LMCategory.find(
+      (kategori) => kategori.catId === categoryId
+    ).catName;
+
+    navigation.setOptions({
+      title: categoryName,
+    });
+  }, [navigation, categoryId]);
 
   function renderDrug(itemData) {
     return (
